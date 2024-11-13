bdce502f6348099e65bfed850f2f7c97bc73c2ec
Mikhael KAMDEM <mikhaelkamdem45@gmail.com>
2024-11-13 16:21:29 -0500

    Footer modifications

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+  siteName: string = environment.siteName;
+  ngOnInit(): void {
 
+  }
 }
